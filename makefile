9c7e142e83f7d0300d3d670236525c248118ba97
Ben <git@othyn.com>
2023-05-22 00:47:25 +0100

    I'm blaming the fact its late, but what on earth am I doing using Python for something like this when a simple bash script would suffice. Talk about over engineering...

--- makefile
+++ makefile
@@ -4,28 +4,6 @@ setup:
 	@ ( test -f brstm_converter && echo "> brstm_converter already compiled." ) || ( g++ -O2 -std=c++0x openrevolution/src/converter.cpp -o brstm_converter -w -Wall && echo "> brstm_converter compiled!" )
 	@ printf "\n"
 
-	@ echo "Setting up Python venv..."
-	@ ( test -d .venv && echo "> .venv already setup." ) || ( python -m venv .venv && python -m pip install --upgrade pip && echo "> .venv setup!" )
-	@ printf "\n"
-
-	@ echo "Updating Python venv pip version..."
-	@ . .venv/bin/activate && python -m pip install --upgrade pip
-	@ printf "\n"
-
-	@ echo "Installing project requirements into Python venv..."
-	@ . .venv/bin/activate && pip install -Ur requirements.txt
-	@ printf "\n"
-
-	@ echo "[[ WARN ]] Remember to run '. .venv/bin/activate' to activate the projects Python venv on your current shell session!"
-	@ printf "\n"
-	@ echo "Done."
-
-.PHONY: reqs
-reqs:
-	@ echo "Updating the projects requirements.txt from Python venv..."
-	@ . .venv/bin/activate && pip freeze > requirements.txt
-	@ printf "\n"
-
 	@ echo "Done."
 
 .PHONY: reset
@@ -34,17 +12,9 @@ reset:
 	@ ( test -f brstm_converter && rm -rf brstm_converter && echo "> brstm_converter deleted." ) || echo "> brstm_converter already deleted."
 	@ printf "\n"
 
-	@ echo "Deactivating Python venv..."
-	@ ( test -d .venv && . .venv/bin/activate && deactivate && echo "> Python venv deactivated." ) || echo "> Python venv already deactivated."
-	@ printf "\n"
-
-	@ echo "Deleting all Python venv files..."
-	@ ( test -d .venv && rm -rf .venv && echo "> Python venv deleted." ) || echo "> Python venv already deleted."
-	@ printf "\n"
-
 	@ echo "Done."
 
 .PHONY: convert
 convert:
 	@ echo "Converting..."
-	@ ( test -d .venv && . .venv/bin/activate && python src/convert.py ) || echo "[[ ERROR ]] Please check the output and if required run 'make setup' if its a dependency issue."
+	@ src/convert.sh
---
+++ src/convert.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+
+# This script is designed to run from the makefile in the root of the project, and thus all paths will be anchored to
+# the root of the project, not src. For that reason, there is no anchoring of the directory paths to the script
+# directory using something like:
+# scriptDir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# Check if the required binary has been compiled
+if [ ! -f brstm_converter ]; then
+    echo "Please run 'make setup' to setup the project first."
+    exit 1
+fi
+
+# Determine find binary to use
+findCmd=find
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    if command -v gfind >/dev/null 2>&1; then
+        findCmd=gfind
+    else
+        echo "[ERR] GNU find is required for this script."
+        echo "[ERR] macOS ships with a BSD version of 'find', which is a pain in the arse."
+        echo "[ERR] Please run 'brew install findutils' first to install GNU find."
+        exit 1
+    fi
+fi
+
+# Gather a list of files to convert, avoiding argument expansion faults with large amounts of files
+filesToConvert=(`${findCmd} in -maxdepth 1 -name "*.bwav"`)
+
+# Get the amount of items in the array for sanity checking and helpful command progress output
+amountOfFilesToConvert=${#filesToConvert[@]}
+
+# Locale format the command output total amount of files number
+amountOfFilesToConvertPretty=$(printf "%'.0f" ${amountOfFilesToConvert})
+
+# Check if there is anything to convert
+if [ ${amountOfFilesToConvert} -eq 0 ]; then
+    echo "Nothing to convert. Please place your source *.bwav files in the 'in' directory first."
+    exit 0
+fi
+
+# Setup a file index ticker, the alternative is to loop the keys in the array with ${!filesToConvert[@]} and instead
+# call array elements when required with the index ${filesToConvert[$i]}. But a ticker seems cleaner...
+currentFileIdx=0
+
+# Convert shit
+for inFilePath in ${filesToConvert[@]}; do
+    # Increment the ticker
+    ((currentFileIdx++))
+
+    # Extract the file name from the in file path so we can generate the outfile path
+    inFileName=$(basename ${inFilePath})
+
+    # Generate the out file path, swapping the file extension
+    outFilePath="out/${inFileName%.bwav}.wav"
+
+    # Locale format the command output current file number
+    currentFileIdxPretty=$(printf "%'.0f" ${currentFileIdx})
+
+    # Using the current length of the finishing number, output and pad the current progress
+    printf "┌[PROG] %${#amountOfFilesToConvertPretty}s / %s\n" ${currentFileIdxPretty} ${amountOfFilesToConvertPretty}
+
+    echo "├[CONV] Converting '${inFilePath}' to '${outFilePath}'..."
+
+    # Sanity check to stop us repeating ourselves, should add a -f command flag to allow overwrite or something
+    if [ -f ${outFilePath} ]; then
+        echo "└[SKIP] Skipped conversion as '${outFilePath}' already exists."
+        continue
+    fi
+
+    # Convert the file, supressing only stdout, we want stderr
+    ./brstm_converter ${inFilePath} -o ${outFilePath} >/dev/null
+
+    echo "└[DONE] Conversion successful."
+    echo ""
+done
+
+# Done
+echo "Done."
